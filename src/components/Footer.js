300104a3135514aba81e75e82ecb263bda629063
Henry Javangwe <hjavangwe@methys.com>
2022-12-03 14:10:32 +0200

    updated the footer to automate the updating of the footer year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,12 @@ import { Container } from "react-bootstrap";
 import '../css/footer.css'
 
 const Footer = () => {
+    const date = new Date();
+    
     return ( 
         <Container fluid className="py-2 Footer_wrapper">
             <div >
-                <p> &copy; Henry Javangwe | All right reserved 2021</p>
+                <p> &copy; Henry Javangwe | All right reserved {date.getFullYear()}</p>
             </div>
         </Container>
      );
